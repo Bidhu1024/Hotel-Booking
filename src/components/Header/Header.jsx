a744c037aa406860bedbf6d639e0363e58b22bf7
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-03 20:10:11 +0530

    23.23

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,8 @@ import {
   faCar,
   faPlane,
   faTaxi,
+  faCalendarDays,
+  faPerson,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
@@ -10,29 +12,53 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-    <div className="headerContainer">
-      <div className="headerList">
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Stays</span>
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flight</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attraction</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxi</span>
+          </div>
         </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faPlane} />
-          <span>Flight</span>
+        <h1 className="headerTitle">A lifetime of Discounts? Its Genius...</h1>
+        <p className="headerDesc">Get rewarded for your travels from Charlie</p>
+        <button className="headerBtn">Register / SignIn</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+             <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 Adults 2 Children 1 room</span>
+          </div>
+          <div className="headerSearchItem">
+      <button className="headerBtn">Search</button>
+          </div>
         </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faCar} />
-          <span>Car</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Attraction</span>
-        </div>
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faTaxi} />
-          <span>Airport Taxi</span>
-        </div>
-      </div>
       </div>
     </div>
   );
