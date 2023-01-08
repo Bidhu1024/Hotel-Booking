8bdc5496596f011ef679d75a4cddfb1b2a27b5a5
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-08 08:50:56 +0530

    45.28

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import './Featured.css'
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className='featured'>
+        <div className="featuredItem">
+            <img src="https://www.hospitalitynet.org/picture/xxl_153130133.jpg?t=1627896887" alt="" />
+            <div className="featuredTitles">
+                <h1>
+                    Bhubaneswar
+                </h1>
+                <h1>
+                    my prop
+                </h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
