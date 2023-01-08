2755cecdc22e2ef9af50435ee6eb556f71c4d201
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-08 11:43:26 +0530

    Home page featured property done

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './FeaturedProperties.css'
+const FeaturedProperties = () => {
+  return (
+    <div className='fp'>
+    <div className="fpItem">
+        <img src="https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI=" alt="" className='fpImg' />
+        <span className='fpName'>Apartments</span>
+        <span className='fpCity'>Bhubaneswar</span>
+        <span className='fpPrice'>Starting from $200</span>
+        <div className="fpRating">
+            <button>8.3</button>
+            <span>Excellent</span>
+            </div>
+        </div>
+        <div className="fpItem">
+        <img src="https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI=" alt="" className='fpImg' />
+        <span className='fpName'>Apartments</span>
+        <span className='fpCity'>Bhubaneswar</span>
+        <span className='fpPrice'>Starting from $200</span>
+        <div className="fpRating">
+            <button>8.3</button>
+            <span>Excellent</span>
+            </div>
+        </div>
+        <div className="fpItem">
+        <img src="https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI=" alt="" className='fpImg' />
+        <span className='fpName'>Apartments</span>
+        <span className='fpCity'>Bhubaneswar</span>
+        <span className='fpPrice'>Starting from $200</span>
+        <div className="fpRating">
+            <button>8.3</button>
+            <span>Excellent</span>
+            </div>
+        </div>
+
+        <div className="fpItem">
+        <img src="https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI=" alt="" className='fpImg' />
+        <span className='fpName'>Apartments</span>
+        <span className='fpCity'>Bhubaneswar</span>
+        <span className='fpPrice'>Starting from $200</span>
+        <div className="fpRating">
+            <button>8.3</button>
+            <span>Excellent</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedProperties
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../../components/Navbar/Navbar'
 import Header from './../../components/Header/Header';
 import Featured from './../../components/Featured/Featured';
 import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from './../../components/featuredProperties/FeaturedProperties';
 const Home = () => {
   return (
     <div>
@@ -20,6 +21,7 @@ Browse Property by List
 <h1 className="homeTitle">
   Homes Guests Love
 </h1>
+<FeaturedProperties />
     </div>
     </div>
   )
