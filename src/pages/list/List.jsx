d1fbd9c86f9dc97e019c4d7b7f90b1626f949029
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-08 12:51:09 +0530

    1.05.55

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "./../../components/Header/Header";
-
+import './List.css'
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+   
+          </div>
+          <div className="listResult">
+       
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
