756a1607fd85ccb38641a17477f536571091f0b7
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-10 06:02:16 +0530

    Single Hotel page start

--- src/pages/SingleHotel/Hotel.jsx
+++ src/pages/SingleHotel/Hotel.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import './Hotel.css'
+import Navbar from './../../components/Navbar/Navbar';
+import Header from './../../components/Header/Header';
 const Hotel = () => {
   return (
-    <div>Hotel</div>
+    <div>
+      <Navbar />
+      <Header type='list' />
+    </div>
   )
 }
 
